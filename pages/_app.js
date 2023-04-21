dd50eb9438c09db99353c7560fc1efa9c35b8431
Olena Hrynykha <olenagrynyha@gmail.com>
2023-04-21 10:23:11 +0300

    add favicon, meta description

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,19 @@ import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import { SSRProvider } from "react-bootstrap";
 import "../styles/global.scss";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <SSRProvider>
+      <Head>
+        <title>Корсет-захід</title>
+        <link rel="shortcut icon" href="/favicon_io/favicon.ico" />
+        <meta
+          name="description"
+          content="Шукаєте ефективне вирішення проблеми з дитячим сколіозом? Корсет-захід пропонує найкращий метод сьогодення - 'Корсети Шено'. Заходьте на сайт та отримуйте більш детальну інфлрмацію!"
+        />
+      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
