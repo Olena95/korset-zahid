6784af45e7202976b0719dcfec0845e460b13eeb
Olena Hrynykha <olenagrynyha@gmail.com>
2023-08-31 20:53:02 +0300

    add viber icon

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -5,55 +5,68 @@ import { useEffect } from "react";
 import MobileMenu from "./MobileMenu";
 
 const Header = () => {
-  const router = useRouter();
+    const router = useRouter();
 
-  // useEffect(() => {
-  //   if (isMenuOpen) {
-  //     setMenuOpen(!isMenuOpen);
-  //   }
-  // }, [router.asPath]);
-  console.log(router);
-  return (
-    <div className="container-fluid">
-      <header>
-        <div className={`row mb-3 main-header active-background`}>
-          <div className={`col-6 col-xs-3 col-md-4 col-lg-3   `}>
-            KORSET-ZAHID
-          </div>
-          <div
-            className="col  col-md-1 col-lg-1 main-header__menu-btn"
-            onClick={() => router.push("/")}
-          >
-            Головна
-          </div>
-          <div
-            className="col  col-md-1 col-lg-1 main-header__menu-btn"
-            onClick={() => router.push("/methodology")}
-          >
-            Методологія
-          </div>
+    // useEffect(() => {
+    //   if (isMenuOpen) {
+    //     setMenuOpen(!isMenuOpen);
+    //   }
+    // }, [router.asPath]);
+    const viberGroup =
+        "https://invite.viber.com/?g2=AQBTNn249om6Vk%2BnMl71CgzcLPXBYNOJX7FHdkkZ2OGx5dMKkJlX5leFyOm2Hx6p";
+    console.log(router);
+    return (
+        <div className="container-fluid">
+            <header>
+                <div className={`row mb-3 main-header active-background`}>
+                    <div className={`col-6 col-xs-3 col-md-4 col-lg-3   `}>
+                        KORSET-ZAHID
+                    </div>
+                    <div
+                        className="col  col-md-1 col-lg-1 main-header__menu-btn"
+                        onClick={() => router.push("/")}
+                    >
+                        Головна
+                    </div>
+                    <div
+                        className="col  col-md-1 col-lg-1 main-header__menu-btn"
+                        onClick={() => router.push("/methodology")}
+                    >
+                        Методологія
+                    </div>
 
-          <div
-            className="col  col-md-2 col-lg-1 main-header__menu-btn"
-            onClick={() => router.push("/services")}
-          >
-            Послуги
-          </div>
+                    <div
+                        className="col  col-md-2 col-lg-1 main-header__menu-btn"
+                        onClick={() => router.push("/services")}
+                    >
+                        Послуги
+                    </div>
 
-          <div
-            className="col  col-md-2 col-lg-1 main-header__menu-btn"
-            onClick={() => router.push("/contacts")}
-          >
-            Контакти
-          </div>
-
-          <div className="col-2 main-header__menuBurger">
-            <MobileMenu />
-          </div>
+                    <div
+                        className="col  col-md-2 col-lg-1 main-header__menu-btn"
+                        onClick={() => router.push("/contacts")}
+                    >
+                        Контакти
+                    </div>
+                    <div
+                        className="col  col-md-2 col-lg-1 main-header__menu-btn"
+                        onClick={() => router.push(viberGroup)}
+                        target="_blank"
+                    >
+                        <Image
+                            src="/images/icons/viber.png"
+                            width={32}
+                            height={32}
+                            alt="viber"
+                        />
+                    </div>
+                    <div className="col-2 main-header__menuBurger">
+                        <MobileMenu />
+                    </div>
+                </div>
+            </header>
         </div>
-      </header>
-    </div>
-  );
+    );
 };
 
 export default Header;
--- components/Header/MobileMenu.js
+++ components/Header/MobileMenu.js
@@ -1,47 +1,58 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-
+import Image from "next/image";
 const MobileMenu = () => {
-  const router = useRouter();
-  const [isMenuOpen, setMenuOpen] = useState();
+    const router = useRouter();
+    const [isMenuOpen, setMenuOpen] = useState();
 
-  useEffect(() => {
-    if (isMenuOpen) {
-      setMenuOpen(!isMenuOpen);
-    }
-  }, [router.asPath]);
+    useEffect(() => {
+        if (isMenuOpen) {
+            setMenuOpen(!isMenuOpen);
+        }
+    }, [router.asPath]);
+    const viberGroup =
+        "https://invite.viber.com/?g2=AQBTNn249om6Vk%2BnMl71CgzcLPXBYNOJX7FHdkkZ2OGx5dMKkJlX5leFyOm2Hx6p";
+    return (
+        <div className="menuToggle ">
+            <input
+                type="checkbox"
+                onChange={(e) => setMenuOpen(e.target.checked)}
+                checked={isMenuOpen}
+            />
 
-  return (
-    <div className="menuToggle ">
-      <input
-        type="checkbox"
-        onChange={(e) => setMenuOpen(e.target.checked)}
-        checked={isMenuOpen}
-      />
+            <span className="burger-icon"></span>
+            <span className="burger-icon"></span>
+            <span className="burger-icon"></span>
 
-      <span className="burger-icon"></span>
-      <span className="burger-icon"></span>
-      <span className="burger-icon"></span>
+            <ul className="menu custom-shadow">
+                <Link href="/">
+                    <li> Головна</li>
+                </Link>
+                <Link href="/methodology">
+                    <li>Методологія</li>
+                </Link>
 
-      <ul className="menu custom-shadow">
-        <Link href="/">
-          <li> Головна</li>
-        </Link>
-        <Link href="/methodology">
-          <li>Методологія</li>
-        </Link>
+                <Link href="/services">
+                    <li>Послуги</li>
+                </Link>
 
-        <Link href="/services">
-          <li>Послуги</li>
-        </Link>
-
-        <Link href="/contacts">
-          <li>Контакти</li>
-        </Link>
-      </ul>
-    </div>
-  );
+                <Link href="/contacts">
+                    <li>Контакти</li>
+                </Link>
+                <Link href={viberGroup} target="_blank">
+                    <li>
+                        <Image
+                            src="/images/icons/viber.png"
+                            width={32}
+                            height={32}
+                            alt="viber"
+                        />
+                    </li>
+                </Link>
+            </ul>
+        </div>
+    );
 };
 
 export default MobileMenu;
